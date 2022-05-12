7e1f4c8b4e5d08496455e2d333db69743cba481e
TuanVOID <102146396+TuanVOID@users.noreply.github.com>
2022-05-12 09:11:22 +0700

    abc

---
+++ company-info.ts
@@ -0,0 +1,32 @@
+let nameMap = new Map();
+nameMap.set("Anand",1001);
+nameMap.set("Bhargavi",1002);
+nameMap.set("Chrestin",1003);
+nameMap.set("Daniel",1004);
+nameMap.set("Esther",1005);
+
+// console.log(nameMap.size);
+// console.log(nameMap.get("Chrestin"));
+// console.log(nameMap.get("Esther"));
+
+// for (let item of nameMap.keys()) {
+//     console.log("Names: "+item);
+// }
+// console.log()
+
+// for (let item of nameMap.values()) {
+//     console.log("ID: ",item);
+// }
+
+// for (let item of nameMap.entries()) {
+//     console.log("entries: ", item[0], item[1]);
+// }
+
+// for (let [key, value] of nameMap) {
+//     console.log("key value pairs: ", key, value);
+// }
+
+// nameMap.delete("Daniel")
+// console.log(nameMap)
+// Clear all the entries of the map
+nameMap.clear();
